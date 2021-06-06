bb8f7904a16e1052e9c63cc9f57f6f32ec821e7b
JulioC-19 <julioalva1992@gmail.com>
2021-06-06 13:54:21 -0400

    This is a file for git testing Initial commit

---
+++ git_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hello!, trying to learn git commands\n :) ");
+    printf("Hey this is making some sense now\n");
+    return 0;
+}
